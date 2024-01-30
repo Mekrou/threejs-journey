0dd79bd7d1cf6d2e55bc5507ee2606e4d13df053
Mekrou <mek.rmills@gmail.com>
2024-01-30 01:09:59 -0500

    setup most of first scene

--- first-test-project/script.js
+++ first-test-project/script.js
@@ -1,4 +1,18 @@
 import * as THREE from 'three'
 console.log(THREE)
 
-console.log("JavaScript is working")
+const sizes = {
+    width: 800,
+    height: 600.
+}
+
+const scene = new THREE.Scene()
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({ color: THREE.Color.NAMES.red})
+
+const box = new THREE.Mesh(geometry, material)
+
+scene.add(box)
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 1000)
+scene.add(camera)
